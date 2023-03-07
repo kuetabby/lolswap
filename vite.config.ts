4eeafb6837cf5c11450b2b8ba7fa748df488d881
Muhamad Ivan <vansmuhammad@gmail.com>
2023-03-07 21:57:05 +0700

    update: global

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: "window",
+			},
+		},
+	},
 	resolve: {
 		alias: [
 			{
