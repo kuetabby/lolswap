362bb475b372e26439cb5dd2c2fb9ac9abc9db52
Muhamad Ivan <vansmuhammad@gmail.com>
2023-03-14 16:57:54 +0700

    add: utils to format token amount

--- src/features/Swap/@utils/index.ts
+++ src/features/Swap/@utils/index.ts
@@ -27,3 +27,46 @@ export const connectors: [MetaMask | WalletConnect | CoinbaseWallet | Network, W
 	[coinbaseWallet, coinbaseWalletHooks],
 	[network, networkHooks],
 ]
+
+export const priceToPreciseFloat = (price: number | undefined) => {
+	if (!price) return undefined
+	const floatForLargerNumbers = parseFloat(price.toFixed(9))
+	if (floatForLargerNumbers < 0.1) {
+		return parseFloat(price.toPrecision(6))
+	}
+	return floatForLargerNumbers
+}
+
+export const formatTransactionAmount = (num: number | undefined | null, maxDigits = 9) => {
+	if (num === 0) return "0.00"
+	if (!num) return ""
+	if (num < 0.00001) {
+		return "<0.00001"
+	}
+	if (num >= 0.00001 && num < 1) {
+		// console.log("< 1")
+		return `${Number(num.toFixed(6)).toLocaleString("en-US", {
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 6,
+		})}`
+	}
+	if (num >= 1 && num < 10000) {
+		// console.log("< 10000")
+		return `${Number(num.toPrecision(6)).toLocaleString("en-US", {
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 6,
+		})}`
+	}
+	if (num >= 10000 && num < 1000000) {
+		// console.log("< 1000000")
+		return `${Number(num.toFixed(2)).toLocaleString("en-US", { minimumFractionDigits: 2 })}`
+	}
+	// For very large numbers, switch to scientific notation and show as much precision
+	// as permissible by maxDigits param.
+	if (num >= Math.pow(10, maxDigits - 1)) {
+		// console.log(Math.pow(10, maxDigits - 1), "math pow")
+		return `${num.toExponential(maxDigits - 3)}`
+	}
+	// console.log("fixed 2")
+	return `${Number(num.toFixed(2)).toLocaleString("en-US", { minimumFractionDigits: 2 })}`
+}
