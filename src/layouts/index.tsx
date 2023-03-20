80770982cba332faef6a1f19e49d44c72de27f7a
Muhamad Ivan <vansmuhammad@gmail.com>
2023-03-20 21:21:05 +0700

    update: display loading indicator when isn't active

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -17,7 +17,7 @@ interface Props {}
 const { Content, Footer } = Layout
 
 const AppLayout: React.FC<Props> = () => {
-	const { account } = useWeb3React()
+	const { account, isActive } = useWeb3React()
 
 	return (
 		<Layout className="app-layout dark-navbar">
@@ -26,8 +26,18 @@ const AppLayout: React.FC<Props> = () => {
 				<Swap />
 			</Content>
 			<Footer className={clsx("app-footer")} style={{ background: "rgb(19, 24, 35)" }}>
-				<GlobalSetting containerClass="flex sm:hidden" />
-				{account ? <AccountWallet containerClass="flex sm:hidden" /> : <Web3Connect containerClass="flex sm:hidden" />}
+				{!isActive && (
+					<div
+						className="inline-block h-8 w-8 m-auto text-blue-500 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+						role="status"
+					/>
+				)}
+				{isActive && (
+					<>
+						<GlobalSetting containerClass="flex sm:hidden" />
+						{account ? <AccountWallet containerClass="flex sm:hidden" /> : <Web3Connect containerClass="flex sm:hidden mr-2" />}
+					</>
+				)}
 			</Footer>
 		</Layout>
 	)
