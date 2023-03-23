8d06bc51d0b5bb9056b9c9bb6825b260ce009ae4
Muhamad Ivan <vansmuhammad@gmail.com>
2023-03-23 14:59:13 +0700

    update: custom tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        'dark-primary': '#06070A',
+        'dark-secondary': 'rgb(19, 24, 35)',
+        'dark-switcher': '#324054',
+        'dark-hover': '#99a1bd14'
+      },
+      boxShadow: {
+        'dark-shadow': 'inset 0 0 0 1px #202835',
+      },
       width: {
-        '30%': "30%"
+        '17.5%': "17.5%",
+        '30%': "30%",
+        "47.5%": "47.5%"
       }
     },
   },
