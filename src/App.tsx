eeaeae81330e981836538502cce2cd539033889f
Muhamad Ivan <vansmuhammad@gmail.com>
2023-03-23 14:59:56 +0700

    update: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-// import { useEffect } from "react"
 // import { useAppDispatch } from "./redux/store"
 // import { useWeb3React } from "@web3-react/core"
 
-import "./App.css"
-
 import AppLayout from "./layouts"
+import { useDarkTheme } from "./shared/hooks/useDarkTheme"
 import { useStoreToken } from "./shared/hooks/useSessionToken"
 
+import "./App.css"
 // import { setFromTradeAddress } from "#/redux/slices/Swap"
 
 function App() {
 	useStoreToken()
+	useDarkTheme()
 	// const { account } = useWeb3React()
 
 	// const dispatch = useAppDispatch()
